ac13140a7f81ea61797b78c9e566507011cba75b
Leticia Alves <130405282+LeticiaOk@users.noreply.github.com>
2024-03-08 16:55:05 -0300

    Add files via upload

---
+++ desafios/desafio013/resolucao/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        .nota{
+            height: 50px;
+            margin: 5px;
+        }
+    </style>
+</head>
+<body>
+    <?php 
+        $saque = $_GET['saque'] ?? '0';
+    ?>
+    <main>
+        <h1>Caixa eletrônico</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="saque">Qual valor você deseja sacar? (R$) <sup>*</sup></label>
+            <input type="number" name="saque" id="saque" step="5" require value="<?=$saque?>">
+            <p style="font-size: 0.6em"><sup>*</sup>Notas disponíveis: R$100, R$50, R$10 e R$5</p>
+            <input type="submit" value="Sacar">
+        </form>
+    </main>
+    <?php 
+        $resto = $saque;
+        // Saque de R$100
+        $tot100 = floor($resto / 100);
+        $resto %= 100;
+
+        // Saque de R$50
+        $tot50 =  floor($resto / 50);
+        $resto %= 50;
+        
+        // Saque de R$10
+        $tot10 = floor($resto / 10);
+        $resto %= 10;
+
+        // Saque de R$5
+        $tot5 = floor($resto / 5);
+        $resto %= 5;
+    ?>
+    <section>
+        <h2>Saque de R$ <?=number_format($saque, 2,',', '.')?> realizado</h2>
+        <p>O caixa eletrônico vai te entregar as seguintes notas:</p>
+        <ul>
+            <li><img src="../imagens/100-reais.jpg" alt="aloalo" class="nota"> x<?=$tot100?> </li>
+            <li><img src="../imagens/50-reais.jpg" alt="" class="nota"> x<?=$tot50?> </li>
+            <li><img src="../imagens/10-reais.jpg" alt="" class="nota"> x<?=$tot10?> </li>
+            <li><img src="../imagens/5-reais.jpg" alt="" class="nota"> x<?=$tot5?> </li>
+        </ul>
+       
+    </section>
+</body>
+</html>
