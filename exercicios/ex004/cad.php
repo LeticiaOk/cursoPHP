1545d651b3851ada8065e1b3ef719fda7c6b67c3
Leticia Alves <130405282+LeticiaOk@users.noreply.github.com>
2024-03-08 17:01:19 -0300

    Add files via upload

---
+++ exercicios/ex004/cad.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado do processamento</h1>
+    </header>
+    <main>
+        <?php 
+            $nome = $_GET["nome"] ??  "sem nome"; // operador de coalescência nula (se tiver nome recebe o valor de nome se não recebe "sem nome")
+            $sobrenome = $_GET['sobrenome'] ?? "desconhecido";
+
+            echo "É um prazer te conhecer, <strong>$nome $sobrenome</strong>! Este é o meu site!";
+        ?>
+
+        <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
+    </main>
+</body>
+</html>
