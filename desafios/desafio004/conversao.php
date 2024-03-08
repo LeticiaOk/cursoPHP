ea9d96d5720b6a9a2bd1194ee71742bbf21bb059
Leticia Alves <130405282+LeticiaOk@users.noreply.github.com>
2024-03-08 16:37:28 -0300

    Add files via upload

---
+++ desafios/desafio004/conversao.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas</h1>
+        <?php 
+            // Cotação vinda da API do Banco Cnetral
+            $início = date("m-d-Y,", strtotime("-7 days"));
+            $fim = date("m-d-Y");
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$início.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+        
+            $dados = json_decode(file_get_contents($url),true);
+        
+            //var_dump($dados);
+        
+            $cotação = $dados["value"][0]["cotacaoCompra"];
+            
+            // Quantos $$ você tem?
+            $real = $_GET["din"] ?? 0;
+
+            // Equivalência em dólar
+            $dólar = $real / $cotação;
+
+            # Jeito 1
+            // Mostrar o resultado
+            // echo "Seus R\$" . number_format($real, 2, ",", ".") . " equeivalem a U\$" .number_format($dólar, 2, ",", ".")
+            // Formata a variavel:
+            // 2 casas decimais
+            // , no lugar do separador decimal
+            // . no lugar do separador de milhar
+            
+            # Jeito 2
+            // Formatação de moedas com internacionalização
+            // Biblioteca (Internallization PHP)
+            $padrão = numfmt_create("pt_BR", NumberFormatter::CURRENCY); //Internacionaliza o código de acordo com o idioma
+            echo "<p>Seus " . numfmt_format_currency($padrão, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrão,$dólar,"USD") . "</strong></p>";
+        ?>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
+    </main>
+    
+</body>
+</html>
